cc7611f7b73a84389e4deda6dcbe8e421ae62a64
Jose Carreres <jcarreres.dev@gmail.com>
2021-12-27 21:15:07 +0100

    functionality page vowel

--- src/js/script.js
+++ src/js/script.js
@@ -17,3 +17,23 @@ artyom.initialize({
 }).catch((err) => {
     console.error("Artyom couldn't be initialized: ", err);
 });
+
+
+// Vowel listen
+document.getElementById("btn-lisen-vowel-a").addEventListener("click", function() {
+    artyom.say("aa");
+}, false);
+document.getElementById("btn-lisen-vowel-e").addEventListener("click", function() {
+    artyom.say("eeee");
+}, false)
+document.getElementById("btn-lisen-vowel-i").addEventListener("click", function() {
+    artyom.say("ii");
+}, false);
+document.getElementById("btn-lisen-vowel-o").addEventListener("click", function() {
+    artyom.say("o");
+}, false)
+document.getElementById("btn-lisen-vowel-u").addEventListener("click", function() {
+    artyom.say("uu");
+}, false);
+
+
